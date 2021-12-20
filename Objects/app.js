5df2961bb20a0db99bbabc14b91b0896754f7b6f
yunhosung <yhs4157@naver.com>
2021-12-20 14:21:19 +0900

    clear chapter 2 JS project

---
+++ Objects/app.js
@@ -0,0 +1,11 @@
+const player = {
+    name : "hosung", 
+    points : 121212, 
+    fat: true,
+
+    sayhello: function(otherPersonsName) {
+        console.log("hello!" + otherpersonsName + "nice to meet you"); 
+    },
+};
+
+player.sayhello("lynn"); 
