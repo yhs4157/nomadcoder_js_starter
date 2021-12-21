9796973921682214180151fa54bd82f67d35f78c
yunhosung <yhs4157@naver.com>
2021-12-21 14:18:58 +0900

    clear chapter 3 Javascript on browser

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 alert("hi"); 
+
+const a = document.getElementById("hello"); 
+
+console.log(a); 
+console.dir(a); 
+
+const b = document.getElementById("hello"); 
+
+console.log(b); 
+console.dir(b); 
