c377c19fba29d6769143041fed1b6f57271ee8e1
yunhosung <yhs4157@naver.com>
2021-12-22 10:39:27 +0900

    clear chapter 5.1 Timeout and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,22 @@
-const clcok = document.querySelector("h2#clock"); 
+const clock = document.querySelector("h2#clock"); 
 
-function sayHello() {
-    console.log("hello"); 
+function getClock() {
+    const date = new Date(); 
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setinterval(sayHello, 5000);
+getClock(); 
+setInterval(getClock, 1000); 
+
+/*
+5.0 
+    setinterval(sayHello, 5000);
+*/
+
+//  setTimeout(sayHello, 5000); 
+
+/*
+5.1 
+    Date를 사용할 것임. 
+*/
+
