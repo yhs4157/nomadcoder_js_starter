3ee7d85bea896958601d34c96031b726c0364be2
yunhosung <yhs4157@naver.com>
2021-12-23 11:02:23 +0900

    add background picture

--- js/background.js
+++ js/background.js
@@ -1,7 +1,14 @@
 const images = [
-    "0.jpeg", 
-    "1.jpeg",
-    "2.jpeg",
+    "0.jpg", 
+    "1.jpg",
+    "2.jpg", 
+    "3.jpg", 
+    "4.jpg", 
+    "5.jpg", 
+    "6.jpg", 
+    "7.jpg", 
+    "8.jpg", 
+    "9.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
