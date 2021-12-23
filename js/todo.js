679c931e831511e5adb026983ea595c34316751a
yunhosung <yhs4157@naver.com>
2021-12-23 11:36:58 +0900

    clear chapter 7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,26 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+function handleDeleteButton(event) {
+    const li = event.target.parentElement; 
+    li.remove(); 
+}
+
 function paintToDo(newTodo) {
     const li = document.createElement("li"); 
+
     const span = document.createElement("span"); 
-    li.appendChild(span); 
     span.innerText = newTodo;
+
+    const button = document.createElement("button"); 
+    button.innerText = "❌";
+    button.addEventListener("click", handleDeleteButton); 
+
+    li.appendChild(span); 
+    li.appendChild(button); 
+    
     toDoList.appendChild(li); 
+    
     console.log(span.innerText);  
 }
 
