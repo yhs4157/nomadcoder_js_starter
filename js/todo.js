40d75510270156388b5cc6d848d794c59a61e5af
yunhosung <yhs4157@naver.com>
2021-12-23 12:07:34 +0900

    clear chapter 7.4 Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos"; 
+
 const toDos = []; 
 
 function saveToDos() {
-    localStorage.setItem("todos", JSON.stringify(toDos)); 
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); 
 }
 
 function handleDeleteButton(event) {
@@ -44,3 +46,12 @@ function handleToDoSubmit(event) {
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit); 
+
+const savedToDos = localStorage.getItem(TODOS_KEY); 
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos); 
+    parsedToDos.forEach((item) =>{
+        console.log(item); 
+    });
+}
